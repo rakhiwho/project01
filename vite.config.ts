7c04b85732c0543011f5112c41f0c2660a2361a5
rakhiwho <rakhydubey04@gamil.com>
2025-12-15 15:54:57 +0530

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    outDir: "dist",  
+  },
+
   server: {
     host: true,
     port: 5173,
-    allowedHosts: "all",
+    allowedHosts: true,
   },
 });
